9a073d63afc1ac831008e325812429dbd0ced727
George0717 <raden7george@gmail.com>
2024-08-12 23:59:42 +0700

    /last update

--- routes/soRoutes.js
+++ routes/soRoutes.js
@@ -5,29 +5,35 @@ const SalesOrder = require('../models/so');
 
 // Create 
 router.post('/', async (req, res) => {
-    const soPost = new SalesOrder({
-        namaPelanggan: req.body.namaPelanggan,
-        alamatPelanggan: req.body.alamatPelanggan,
-        tanggalPO: req.body.tanggalPO,
-        nomorPO: req.body.nomorPO,
-        nomorSO: req.body.nomorSO,
-        barang: req.body.barang,
-        subTotal: req.body.subTotal,
-        diskon: req.body.diskon,
-        uangMuka: req.body.uangMuka,
-        ppn: req.body.ppn,
-        totalBayar: req.body.totalBayar,
-        tipePembayaran: req.body.tipePembayaran,
-        jadwalPembayaran: req.body.jadwalPembayaran,
-    });
-
     try {
+        const lastSalesOrder = await SalesOrder.findOne().sort({ nomorSO: -1 }).exec();
+        const lastSoNumber = lastSalesOrder ? lastSalesOrder.nomorSO : "SOSPA00";
+        const numberPart = parseInt(lastSoNumber.replace("SOSPA", "")) + 1;
+        const newSoNumber = `SOSPA${numberPart.toString().padStart(2, "0")}`;
+
+        const soPost = new SalesOrder({
+            namaPelanggan: req.body.namaPelanggan,
+            alamatPelanggan: req.body.alamatPelanggan,
+            tanggalPO: req.body.tanggalPO,
+            nomorPO: req.body.nomorPO,
+            nomorSO: newSoNumber, // Gunakan nomorSO baru di sini
+            barang: req.body.barang,
+            subTotal: req.body.subTotal,
+            diskon: req.body.diskon,
+            uangMuka: req.body.uangMuka,
+            ppn: req.body.ppn,
+            totalBayar: req.body.totalBayar,
+            tipePembayaran: req.body.tipePembayaran,
+            jadwalPembayaran: req.body.jadwalPembayaran,
+        });
+
         const salesOrder = await soPost.save();
         response(201, salesOrder, "berhasil", res);
     } catch (error) {
         res.json({ message: error.message });
     }
 });
+
 
 // Read
 router.get('/', async (req, res) => {
@@ -77,17 +83,16 @@ router.delete('/:salesOrderID', async (req, res) => {
 
 router.get('/last', async (req, res) => {
     try {
-        const lastSalesOrder = await SalesOrder.findOne().sort({ _id: -1 }).exec();
-        console.log("Last Sales Order:", lastSalesOrder); // Log data yang diambil
+        const lastSalesOrder = await SalesOrder.findOne().sort({ nomorSO: -1 }).exec();
         if (!lastSalesOrder) {
             return response(200, { nomorSO: "SOSPA00" }, "No Sales Order found", res);
         }
         response(200, { nomorSO: lastSalesOrder.nomorSO }, "Berhasil", res);
     } catch (error) {
-        console.error("Error fetching last Sales Order:", error);
         res.json({ message: error.message });
     }
 });
+
 
 
 module.exports = router;
